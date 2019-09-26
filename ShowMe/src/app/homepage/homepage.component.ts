4de6583f62cff7c602514b237f2adf8ede83a2f3
David Sousa <xipiro15@gmail.com>
2019-09-26 01:23:26 +0000

    created navbar component CLI

---
+++ ShowMe/src/app/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+//<h2>Homepage text </h2>
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',  
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
